2d78e8bb798d630f116d3b3e80f2645448ea83b1
Mariano <136939689+MareanoSG@users.noreply.github.com>
2023-10-12 04:00:47 -0300

    Update bingoPrueba4.js

--- Juegos/Bingo/JS/bingoPrueba4.js
+++ Juegos/Bingo/JS/bingoPrueba4.js
@@ -3,6 +3,7 @@ const betInput = document.querySelector(".bet-input");
 let user = JSON.parse(localStorage.getItem('user'));
 const celdasUsuario = document.querySelectorAll(".tdUsuario");
 
+
 let filaCantada = false;
 let bingoCantado = false;
 //SE ABRE SECCIÓN CARTON USUARIO// 
@@ -39,9 +40,80 @@ let nFila3Usuario_67_69 = Math.floor(Math.random() * 3) + 67;
 let nFila3Usuario_77_79 = Math.floor(Math.random() * 3) + 77;
 let nFila3Usuario_87_90 = Math.floor(Math.random() * 4) + 87;
 
-const fila1Usuario = [nFila1Usuario_1_3, nFila1Usuario_10_13, null, nFila1Usuario_30_33, null, nFila1Usuario_50_53, nFila1Usuario_60_63, null, null]
-const fila2Usuario = [null, nFila2Usuario_14_16, nFila2Usuario_24_26, null, nFila2Usuario_44_46, nFila2Usuario_54_56, null, null, nFila2Usuario_84_86]
-const fila3Usuario = [nFila3Usuario_7_9, null, nFila3Usuario_27_29, nFila3Usuario_37_39, null, null, nFila3Usuario_67_69, nFila3Usuario_77_79, null]
+
+const fila1Usuario = [nFila1Usuario_1_3, nFila1Usuario_10_13, nFila1Usuario_20_23, nFila1Usuario_30_33, nFila1Usuario_40_43, nFila1Usuario_50_53, nFila1Usuario_60_63, nFila1Usuario_70_73, nFila1Usuario_80_83];
+
+// Obtener índices de posiciones no nulas
+const nonNullIndices4 = fila1Usuario.reduce((indices, el, index) => {
+  if (el !== null) {
+    indices.push(index);
+  }
+  return indices;
+}, []);
+
+// Barajar aleatoriamente los índices no nulos
+for (let i = nonNullIndices4.length - 1; i > 0; i--) {
+  const j = Math.floor(Math.random() * (i + 1));
+  [nonNullIndices4[i], nonNullIndices4[j]] = [nonNullIndices4[j], nonNullIndices4[i]];
+}
+
+// Reemplazar cuatro valores no nulos con null
+for (let i = 0; i < 4; i++) {
+  const indexToReplace = nonNullIndices4[i];
+  fila1Usuario[indexToReplace] = null;
+}
+
+console.log(fila1Usuario);
+
+
+const fila2Usuario = [nFila2Usuario_4_6, nFila2Usuario_14_16, nFila2Usuario_24_26, nFila2Usuario_34_36, nFila2Usuario_44_46, nFila2Usuario_54_56, nFila2Usuario_64_66, nFila2Usuario_74_76, nFila2Usuario_84_86];
+
+// Obtener índices de posiciones no nulas
+const nonNullIndices5 = fila2Usuario.reduce((indices, el, index) => {
+  if (el !== null) {
+    indices.push(index);
+  }
+  return indices;
+}, []);
+
+// Barajar aleatoriamente los índices no nulos
+for (let i = nonNullIndices5.length - 1; i > 0; i--) {
+  const j = Math.floor(Math.random() * (i + 1));
+  [nonNullIndices5[i], nonNullIndices5[j]] = [nonNullIndices5[j], nonNullIndices5[i]];
+}
+
+// Reemplazar cuatro valores no nulos con null
+for (let i = 0; i < 4; i++) {
+  const indexToReplace = nonNullIndices5[i];
+  fila2Usuario[indexToReplace] = null;
+}
+
+console.log(fila2Usuario);
+
+
+const fila3Usuario = [nFila3Usuario_7_9, nFila3Usuario_17_19, nFila3Usuario_27_29, nFila3Usuario_37_39, nFila3Usuario_47_49, nFila3Usuario_57_59, nFila3Usuario_67_69, nFila3Usuario_77_79, nFila3Usuario_87_90];
+
+// Obtener índices de posiciones no nulas
+const nonNullIndices6 = fila3Usuario.reduce((indices, el, index) => {
+  if (el !== null) {
+    indices.push(index);
+  }
+  return indices;
+}, []);
+
+// Barajar aleatoriamente los índices no nulos
+for (let i = nonNullIndices6.length - 1; i > 0; i--) {
+  const j = Math.floor(Math.random() * (i + 1));
+  [nonNullIndices6[i], nonNullIndices6[j]] = [nonNullIndices6[j], nonNullIndices6[i]];
+}
+
+// Reemplazar cuatro valores no nulos con null
+for (let i = 0; i < 4; i++) {
+  const indexToReplace = nonNullIndices6[i];
+  fila3Usuario[indexToReplace] = null;
+}
+
+console.log(fila3Usuario);
 
 const cartonUsuario = [fila1Usuario, fila2Usuario, fila3Usuario]
 
@@ -82,9 +154,79 @@ let nFila3Bot_67_69 = Math.floor(Math.random() * 3) + 67;
 let nFila3Bot_77_79 = Math.floor(Math.random() * 3) + 77;
 let nFila3Bot_87_90 = Math.floor(Math.random() * 4) + 87;
 
-const fila1Bot = [nFila1Bot_1_3, nFila1Bot_10_13, null, null, nFila1Bot_40_43, nFila1Bot_50_53, null, nFila1Bot_70_73, null]
-const fila2Bot = [null, nFila2Bot_14_16, nFila2Bot_24_26, null, nFila2Bot_44_46, nFila2Bot_54_56, null, null, nFila2Bot_84_86]
-const fila3Bot = [nFila3Bot_7_9, nFila3Bot_17_19, null, nFila3Bot_37_39, null, null, nFila3Bot_67_69, null, nFila3Bot_87_90]
+const fila1Bot = [nFila1Bot_1_3, nFila1Bot_10_13, nFila1Bot_20_23, nFila1Bot_30_33, nFila1Bot_40_43, nFila1Bot_50_53, nFila1Bot_60_63, nFila1Bot_70_73, nFila1Bot_80_83];
+
+// Obtener índices de posiciones no nulas
+const nonNullIndices = fila1Bot.reduce((indices, el, index) => {
+  if (el !== null) {
+    indices.push(index);
+  }
+  return indices;
+}, []);
+
+// Barajar aleatoriamente los índices no nulos
+for (let i = nonNullIndices.length - 1; i > 0; i--) {
+  const j = Math.floor(Math.random() * (i + 1));
+  [nonNullIndices[i], nonNullIndices[j]] = [nonNullIndices[j], nonNullIndices[i]];
+}
+
+// Reemplazar cuatro valores no nulos con null
+for (let i = 0; i < 4; i++) {
+  const indexToReplace = nonNullIndices[i];
+  fila1Bot[indexToReplace] = null;
+}
+
+
+const fila2Bot = [nFila2Bot_4_6, nFila2Bot_14_16, nFila2Bot_24_26, nFila2Bot_34_36, nFila2Bot_44_46, nFila2Bot_54_56, nFila2Bot_64_66, nFila2Bot_74_76, nFila2Bot_84_86];
+
+// Obtener índices de posiciones no nulas
+const nonNullIndices2 = fila2Bot.reduce((indices, el, index) => {
+  if (el !== null) {
+    indices.push(index);
+  }
+  return indices;
+}, []);
+
+// Barajar aleatoriamente los índices no nulos
+for (let i = nonNullIndices2.length - 1; i > 0; i--) {
+  const j = Math.floor(Math.random() * (i + 1));
+  [nonNullIndices2[i], nonNullIndices2[j]] = [nonNullIndices2[j], nonNullIndices2[i]];
+}
+
+// Reemplazar cuatro valores no nulos con null
+for (let i = 0; i < 4; i++) {
+  const indexToReplace = nonNullIndices2[i];
+  fila2Bot[indexToReplace] = null;
+}
+
+console.log(fila2Bot);
+
+
+
+const fila3Bot = [nFila3Bot_7_9, nFila3Bot_17_19, nFila3Bot_27_29, nFila3Bot_37_39, nFila3Bot_47_49, nFila3Bot_57_59, nFila3Bot_67_69, nFila3Bot_77_79, nFila3Bot_87_90];
+
+// Obtener índices de posiciones no nulas
+const nonNullIndices3 = fila3Bot.reduce((indices, el, index) => {
+  if (el !== null) {
+    indices.push(index);
+  }
+  return indices;
+}, []);
+
+// Barajar aleatoriamente los índices no nulos
+for (let i = nonNullIndices3.length - 1; i > 0; i--) {
+  const j = Math.floor(Math.random() * (i + 1));
+  [nonNullIndices3[i], nonNullIndices3[j]] = [nonNullIndices3[j], nonNullIndices3[i]];
+}
+
+// Reemplazar cuatro valores no nulos con null
+for (let i = 0; i < 4; i++) {
+  const indexToReplace = nonNullIndices3[i];
+  fila3Bot[indexToReplace] = null;
+}
+
+console.log(fila3Bot);
+
 
 const cartonBot = [fila1Bot, fila2Bot, fila3Bot]
 
@@ -111,6 +253,11 @@ let numerosSalientes = [];
             
             // Aquí puedes realizar cualquier acción adicional con el número generado
             console.log("Número generado: " + numeroAleatorio);
+            const utterance = new SpeechSynthesisUtterance(numeroAleatorio)
+            utterance.pitch = 10
+            utterance.rate = 0.8
+            utterance.lang = "es-AR"
+            speechSynthesis.speak(utterance)
 
             const numeroAleatorioElement = document.getElementById("numeroAleatorio");
             numeroAleatorioElement.textContent = "" + numeroAleatorio;
@@ -139,21 +286,51 @@ let numerosSalientes = [];
             }
 
             //Comprueba si hay línea en alguno de los cartones del usuario o bot
+            const audioAlerta = document.getElementById("audio-alerta");
             if (filaCantada == false) {
                 if(fila1Bot.every(num => num === null || numerosSalientes.includes(num))){
                     console.log("¡LÍNEA EN LA PRIMERA FILA DEL BOT!");
+                    const mensaje = document.createElement("p");
+                    mensaje.classList.add("perdiste");
+                    mensaje.textContent="!LINEA DEL BOT!";
+                    audioAlerta.play();
+                    mensajeContainer.appendChild(mensaje);
+                    setTimeout(function () {
+                        mensaje.classList.remove("perdiste");
+                        mensajeContainer.removeChild(mensaje);
+                    }, 3000)
                     filaCantada = true; // Marcar que la fila se ha cantado
                         updateUserPoints(-document.querySelector(".bet-input").value/4)
                 }
 
                 else if(fila2Bot.every(num => num === null || numerosSalientes.includes(num))){
                     console.log("¡LÍNEA EN LA SEGUNDA FILA DEL BOT!");
+                    console.log("¡LÍNEA EN LA PRIMERA FILA DEL BOT!");
+                    const mensaje = document.createElement("p");
+                    mensaje.classList.add("perdiste");
+                    mensaje.textContent="!LINEA DEL BOT!";
+                    audioAlerta.play();
+                    mensajeContainer.appendChild(mensaje);
+                    setTimeout(function () {
+                        mensaje.classList.remove("perdiste");
+                        mensajeContainer.removeChild(mensaje);
+                    }, 3000)
                     filaCantada = true; // Marcar que la fila se ha cantado
                         updateUserPoints(-document.querySelector(".bet-input").value/4)
                 }
 
                 else if(fila3Bot.every(num => num === null || numerosSalientes.includes(num))){
                     console.log("¡LÍNEA EN LA TERCERA FILA DEL BOT!");
+                    console.log("¡LÍNEA EN LA PRIMERA FILA DEL BOT!");
+                    const mensaje = document.createElement("p");
+                    mensaje.classList.add("perdiste");
+                    mensaje.textContent="!LINEA DEL BOT!";
+                    audioAlerta.play();
+                    mensajeContainer.appendChild(mensaje);
+                    setTimeout(function () {
+                        mensaje.classList.remove("perdiste");
+                        mensajeContainer.removeChild(mensaje);
+                    }, 3000)
                     filaCantada = true; // Marcar que la fila se ha cantado
                         updateUserPoints(-document.querySelector(".bet-input").value/4)
                 }
@@ -175,13 +352,16 @@ let numerosSalientes = [];
                     setTimeout(function () {
                         mensaje.classList.remove("perdiste");
                         mensajeContainer.removeChild(mensaje);
-                    }, 2000);
+                        audioPerdiste.play();
+                    }, 3000);
                     
 
                     // Deshabilitar el botón de "Generar Número Aleatorio"
                     document.getElementById("generarNumero").disabled = true;
                 }
         }
+
+
 
         function actualizarCartones() {
             // Obtén los elementos div de los cartones por sus identificadores
@@ -216,7 +396,7 @@ let numerosSalientes = [];
                     }
                     tablaHTML += '>';
                     if (carton[i][j] === null) {
-                        tablaHTML += ''; // Mostrar 'X' en celdas vacías
+                        tablaHTML += 'X'; // Mostrar 'X' en celdas vacías
                     } else {
                         tablaHTML += carton[i][j]; // Mostrar el número
                     }
@@ -280,18 +460,23 @@ function comprobarTabla(){
 numerosDel1Al90.forEach((numero) => {
     const idUnico = `numero_${numero}`;
     const celda = document.getElementById(idUnico);
-
+    const audioFall = document.getElementById("audio-fall")
+    
+    setTimeout(function () {
+        if (numero === numeroAleatorio) {
+            // Si coincide, cambiar el fondo a rojo
+            celda.style.backgroundColor = 'red';
+            audioFall.play();
+        }
+    }, 900)
     // Comprobar si el número coincide con el número saliente
-    if (numero === numeroAleatorio) {
-        // Si coincide, cambiar el fondo a rojo
-        celda.style.backgroundColor = 'red';
-    }
 });
 }
         //se empiezan a comparar las tablas del usuario
         const cantidadDeCeldas = 27;
 
 // Recorrer un bucle para generar los eventos de click para cada celda
+const audioCheck = document.getElementById("audio-check")
 for (let i = 0; i < cantidadDeCeldas; i++) {
     const celdaUsuario = document.getElementById(`celdaUsuario_${i}`);
     celdaUsuario.dataset.coincidencia = "false"; // Inicialmente, establece el atributo a "false"
@@ -301,15 +486,15 @@ for (let i = 0; i < cantidadDeCeldas; i++) {
     }
 
     celdaUsuario.addEventListener("click", function () {
-        if (celdaUsuario.textContent == numeroAleatorio) {
-            console.log("Número coincidente");
-            celdaUsuario.dataset.coincidencia = "true"; // Establece el atributo a "true" cuando coincide
-            // Luego, puedes usar celdaUsuario.dataset.coincidencia para rastrear las coincidencias
-        }
-        
-        else{
-            console.log("Nada");
-        }
+        const numeroCelda = parseInt(celdaUsuario.textContent); // Convierte el contenido de la celda en un número
+
+    if (numerosSalientes.includes(numeroCelda)) {
+        console.log("Número coincidente");
+        celdaUsuario.dataset.coincidencia = "true"; // Establece el atributo a "true" cuando coincide
+        audioCheck.play();
+    } else {
+        console.log("Nada");
+    }
     });
 }
 
@@ -333,6 +518,10 @@ for (let i = 0; i < cantidadDeCeldas; i++) {
      
  }
 }
+const audioGanaste = document.getElementById("audio-ganaste");
+const audioPerdiste = document.getElementById("audio-perdiste");
+const audioAlerta= document.getElementById("audio-alerta");
+
 const botonFila = document.getElementById("fila")
 const botonBingo = document.getElementById("cantarBingo")
 
@@ -359,15 +548,17 @@ botonFila.addEventListener("click", function () {
 
     
         if (fila1Coincide || fila2Coincide || fila3Coincide) {
+            console.log("fila")
             const mensaje = document.createElement("p");
             mensaje.classList.add("ganaste");
             mensaje.textContent="!LINEA DEL USUARIO!";
+            audioGanaste.play();
             mensajeContainer.appendChild(mensaje);
             // Deshabilitar el botón de "fila" después de cantar "LÍNEA"
             botonFila.disabled = true;
             filaCantada = true
                 updateUserPoints(document.querySelector(".bet-input").value/4)
-
+            
             setTimeout(function () {
                 mensaje.classList.remove("ganaste");
                 mensajeContainer.removeChild(mensaje);
@@ -375,13 +566,14 @@ botonFila.addEventListener("click", function () {
         } else {
             const mensaje = document.createElement("p")
             mensaje.classList.add("perdiste");
-            mensaje.textContent="!NO HAY LINEA EN EL CARTON!";
+            mensaje.textContent="!NO HAY FILA EN EL CARTON!";
             mensajeContainer.appendChild(mensaje);
+            audioAlerta.play();
 
             setTimeout(function () {
                 mensaje.classList.remove("perdiste");
                 mensajeContainer.removeChild(mensaje);
-            }, 2000);
+            }, 1000);
         }
         
     }
@@ -400,6 +592,7 @@ botonBingo.addEventListener("click", function (){
         mensaje.classList.add("ganaste");
         mensaje.textContent="!BINGO DEL USUARIO!";
         mensajeContainer.appendChild(mensaje);
+        audioGanaste.play();
 
         botonBingo.disabled = true;
         bingoCantado = true;
@@ -416,17 +609,31 @@ botonBingo.addEventListener("click", function (){
         mensaje.textContent="!NO HAY BINGO!";
         mensajeContainer.appendChild(mensaje);
 
+        audioAlerta.play();
         setTimeout(function () {
             mensaje.classList.remove("perdiste");
             mensajeContainer.removeChild(mensaje);
-                
-        }, 2000);
+        }, 1000);
     }
 }
 })
-    
+
+    const audioBouncingBalls = document.getElementById("audioBalls");
+
     // Asocia la función generarNumeroAleatorio al evento click del botón
-    document.getElementById("generarNumero").addEventListener("click", generarNumeroAleatorio);
+    document.getElementById("generarNumero").addEventListener("click", function () {
+        // Deshabilita el botón mientras se genera el número
+        audioBouncingBalls.play();
+        document.getElementById("generarNumero").disabled = true;
+    
+        // Agrega un retraso de 3 segundos antes de generar el número
+        setTimeout(function () {
+            generarNumeroAleatorio();
+            
+            // Habilita nuevamente el botón después de generar el número
+            document.getElementById("generarNumero").disabled = false;
+        }, 3000); // 3000 milisegundos (3 segundos)
+    });
 
 // Function to update user points using fetch
 async function updateUserPoints(pointsToAddOrSubtract) {
