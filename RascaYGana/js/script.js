c0d174a45aad881765841b4593f1a28e40fed1ff
Gianluca-V <107764977+Gianluca-V@users.noreply.github.com>
2023-10-12 14:01:04 -0300

    changed accepted ports on server to *, changed images on cupgame and scratch and win

---
+++ RascaYGana/js/script.js
@@ -0,0 +1,259 @@
+let user = JSON.parse(localStorage.getItem('user'));
+const betInput = document.querySelector(".bet-input");
+
+const resultDiv = document.getElementById("result-div")
+let finished = false;
+
+let numbers;
+let ticketNumbers;
+
+let canvas = document.getElementById("canvas");
+let context = canvas.getContext("2d");
+let painting = new Image();
+painting.src = "resources/ticket-paint.png";
+painting.onload = () => {
+    const rect = canvas.getBoundingClientRect();
+    canvas.width = rect.width;
+    canvas.height = rect.height;
+    context.drawImage(painting, 0, 0);
+
+    //Da un random en el ticket
+    numbers = []
+    ticketNumbers = Array.from(document.getElementsByClassName("ticket-number"))
+    ticketNumbers.forEach(element => {
+        let randomNumber = Math.floor(Math.random() * 4 + 1)
+        element.textContent = randomNumber
+        element.style.color = getRandomColor();
+        numbers.push(randomNumber) //es una funcion por ende va con parentesis
+        //playScratchSound();
+    });
+};
+
+
+
+let isPainting = false;
+let prevX, prevY; // Previous mouse position
+
+function scratchPaint(event) {
+    if (isPainting && !finished) {
+        playScratchSound()
+        let radius = 50;
+        let rect = canvas.getBoundingClientRect();
+
+
+        if (event.type === 'mousemove') {
+            x = event.clientX - rect.left;
+            y = event.clientY - rect.top;
+        } else if (event.type === 'touchmove') {
+            x = event.touches[0].clientX - rect.left;
+            y = event.touches[0].clientY - rect.top;
+        }
+        context.save(); // Save the current context state
+
+        context.beginPath();
+        context.arc(x, y, radius, 0, Math.PI * 2);
+        context.clip();
+        context.clearRect(0, 0, canvas.width, canvas.height);
+        context.closePath();
+
+        context.restore(); // Restore the previous context state
+
+        // Check if the previous position is available
+        if (prevX !== undefined && prevY !== undefined) {
+            // Calculate the distance between the current and previous position
+            const dx = x - prevX;
+            const dy = y - prevY;
+            const distance = Math.sqrt(dx * dx + dy * dy);
+
+            // Calculate the number of steps for smoothing
+            const steps = Math.ceil(distance / (radius / 2));
+
+            // Perform line interpolation for smoother scratching
+            for (let i = 0; i < steps; i++) {
+                const interpX = prevX + (dx / steps) * i;
+                const interpY = prevY + (dy / steps) * i;
+                context.clearRect(interpX - radius / 2, interpY - radius / 2, radius, radius);
+            }
+        } else {
+            // Clear the current position without interpolation for the first point
+            context.clearRect(x - radius / 2, y - radius / 2, radius, radius);
+        }
+
+        // Update the previous position
+        prevX = x;
+        prevY = y;
+
+        const imageData = context.getImageData(0, 0, canvas.width, canvas.height);
+        const data = imageData.data;
+        const pixelCount = data.length / 4;
+        let clearedPixelCount = 0;
+
+        for (let i = 0; i < data.length; i += 4) {
+            // Check if the pixel is fully transparent
+            if (data[i + 3] === 0) {
+                clearedPixelCount++;
+            }
+        }
+
+        if ((clearedPixelCount / pixelCount) * 100 < 99) return;
+
+        finished = true;
+        if (numbers.every((num) => num == numbers[0])) {
+            resultDiv.innerHTML += '<img src="resources/win-img.png" alt="" id="result-img">'
+            document.getElementById("my-canvas").style.display = "block";
+            playWinSound();
+            updateUserPoints(betInput.value)
+            actualizarPuntos();
+        }
+        else {
+            resultDiv.innerHTML += '<img src="resources/lose-img.png" alt="" id="result-img">'
+            document.getElementById("my-canvas").style.display = "none";
+            playLoseSound();
+            updateUserPoints(-betInput.value)
+            actualizarPuntos();
+        }
+        setTimeout(() => {
+            resetGame()
+        }, 3000)
+    }
+}
+
+function resetGame() {
+    document.getElementById("my-canvas").style.display = "none";
+    finished = false
+    numbers = []
+    resultDiv.innerHTML = ""
+    ticketNumbers.forEach(element => {
+        let randomNumber = Math.floor(Math.random() * 4 + 1)
+        element.textContent = randomNumber
+        element.style.color = getRandomColor();
+        numbers.push(randomNumber) //es una funcion por ende va con parentesis
+        //playScratchSound();
+    });
+    let context = canvas.getContext("2d");
+    let painting = new Image();
+    painting.src = "resources/ticket-paint.png";
+    painting.onload = () => {
+        const rect = canvas.getBoundingClientRect();
+        canvas.width = rect.width;
+        canvas.height = rect.height;
+        context.drawImage(painting, 0, 0);
+    };
+}
+
+canvas.addEventListener("mousedown", function (event) {
+    isPainting = true;
+    prevX = undefined; // Reset the previous position
+    prevY = undefined;
+});
+canvas.addEventListener("mouseup", function (event) {
+    isPainting = false;
+});
+
+canvas.addEventListener("mousemove", scratchPaint);
+
+
+canvas.addEventListener("touchstart", function (event) {
+    event.preventDefault(); // Prevent default touch behavior
+    isPainting = true;
+    prevX = undefined; // Reset the previous position
+    prevY = undefined;
+});
+
+canvas.addEventListener("touchend", function (event) {
+    event.preventDefault(); // Prevent default touch behavior
+    isPainting = false;
+});
+
+canvas.addEventListener("touchmove", scratchPaint);
+
+
+
+function playWinSound() {
+    const winSound = document.getElementById('winSound');
+    winSound.currentTime = 0; // Reinicia el audio a la posición inicial
+    winSound.play();
+}
+
+function playLoseSound() {
+    const loseSound = document.getElementById('loseSound');
+    loseSound.currentTime = 0;
+    loseSound.play();
+}
+
+function playScratchSound() {
+    const scratchSound = document.getElementById('scratchSound');
+    scratchSound.currentTime = 0;
+    scratchSound.play();
+}
+
+function getRandomColor() {
+    const colors = ["red", "orange", "yellow", "green", "black", "purple", "pink", "violet", "magenta", "lime", "hot pink", "gold", "fuchsia"];
+    let color = colors[Math.floor(Math.random() * colors.length)];
+
+    return color;
+}
+
+// Function to update user points using fetch
+async function updateUserPoints(pointsToAddOrSubtract) {
+    const apiUrl = "http://localhost/server/";
+    const requestOptions = {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            username: user.id,
+            points: pointsToAddOrSubtract,
+        }),
+    };
+
+    try {
+        response = await fetch(apiUrl, requestOptions)
+
+        if (response.ok) {
+            // Points updated successfully
+            console.log("Points updated successfully");
+            const id = user.id;
+            const points = await getUserPoints(id);
+            user = { id, points };
+            localStorage.setItem('user', JSON.stringify(user));
+        } else {
+            // Handle the error here
+            console.error("Error updating points");
+        }
+    }
+    catch (error) {
+        // Handle network or other errors
+        console.error("Error updating points:", error);
+    }
+}
+
+async function getUserPoints(username) {
+    const apiUrl = `http://localhost/server/?username=${username}`;
+
+    try {
+        const response = await fetch(apiUrl);
+        if (response.ok) {
+            const data = await response.json();
+            return data.points;
+        } else {
+            // Handle the error here
+            console.error("Error getting user points");
+            return null;
+        }
+    } catch (error) {
+        // Handle network or other errors
+        console.error("Error getting user points:", error);
+        throw error;
+    }
+}
+actualizarPuntos();
+async function actualizarPuntos() {
+  try {
+    const puntos = await getUserPoints(user.id);
+    document.getElementById("ptstotales__text").textContent = 'Puntos: ' + puntos;
+  } catch (error) {
+    console.error("Error al obtener los puntos:", error);
+  }
+}
---
+++ Vasos/js/main.js
@@ -0,0 +1,199 @@
+let firstTime = true; // Flag to track if it's the first time playing the game
+let cupNumber = 3; // Number of cups in the game
+let probabilityOffset = 2; // Offset for the probability of adding a ball to a cup
+const cupHTML = '<div class="cup-container" draggable="false"><div class="cup"><img src="resources/cup-img.png" alt="cup-img" draggable="false"></div></div>'; //cup html code
+const resultDiv = document.getElementById("result-div")
+let cupsContainer = document.getElementById("cups-container"); // Container element for the cups
+let cupContainer; // Array to store the cup container elements
+const betInput = document.querySelector(".bet-input");
+
+let user = JSON.parse(localStorage.getItem('user'));
+
+function shuffle() {
+  const items = gsap.utils.toArray(".cup-container"); // Convert cup container elements into an array
+
+  // Get the state of the cup containers
+  const state = Flip.getState(items);
+
+  // Do the actual shuffling of the cup containers
+  for (let i = items.length; i >= 0; i--) {
+    cupsContainer.appendChild(cupsContainer.children[Math.random() * i | 0]);
+    playCupMoveSound();
+  }
+
+  // Animate the change using the Flip library
+  Flip.from(state, {
+    absolute: true
+  });
+}
+
+function createCups() {
+  // Create cup containers
+  for (let index = 0; index < cupNumber; index++) {
+    cupsContainer.innerHTML += cupHTML;
+  }
+
+  // Get cup elements and cup container elements
+  const cups = document.getElementsByClassName("cup");
+  cupContainer = document.getElementsByClassName("cup-container");
+
+  let selected = false;
+
+  let cupContainerArray = Array.from(cupContainer);
+
+  if (!firstTime) {
+    // Add motion blur cups to existing cup containers
+    const blurCupHTML = '<div class="motion-blur-cup cup" draggable="false"><img src="resources/cup-img.png" alt="cup-img" draggable="false"></div>';
+    for (let index = 0; index < cupContainerArray.length; index++) {
+      cupContainerArray[index].innerHTML += blurCupHTML;
+      cupContainerArray[index].innerHTML += blurCupHTML;
+    }
+
+    // Create a timeline for cup shuffling animation
+    const totalIterations = Math.floor(Math.random() * 7) + 5;
+    const cupShuffle = gsap.timeline({
+      repeat: totalIterations,
+      repeatDelay: 0.2,
+    });
+
+    // Remove motion blur cups after shuffling animation is completed
+    cupShuffle.eventCallback("onComplete", () => {
+      Array.from(document.getElementsByClassName("motion-blur-cup")).forEach(cup => cup.remove());
+    });
+
+    cupShuffle.call(shuffle); // Call the shuffle function
+  }
+
+  firstTime = false;
+
+  // Add click event listener to each cup container
+  cupContainerArray.forEach((element) => {
+    element.addEventListener('click', () => {
+      if (!selected) {
+        let cup = element.querySelector(".cup");
+        cup.classList.add("cup-animation");
+        selected = true;
+        playCupSound();
+        // Add ball randomly to a cup container
+        //if win
+        if (Math.floor(Math.random() * (cupNumber + probabilityOffset)) == 1) {
+          element.innerHTML += '<div class="ball"><img src="resources/ball-img.png" alt="ball-img" draggable="false"></div>';
+          resultDiv.innerHTML += '<img src="resources/win-img.png" alt="" id="result-img">'
+          playWinSound();
+          updateUserPoints(betInput.value)
+          actualizarPuntos();
+        } //if loose
+        else {
+          resultDiv.innerHTML += '<img src="resources/lose-img.png" alt="" id="result-img">'
+          playLoseSound();
+          updateUserPoints(-betInput.value)
+          actualizarPuntos();
+        }
+        // Reset the game after 3 seconds
+        setTimeout(() => {
+          deleteCups();
+          createCups();
+          resultDiv.innerHTML = ""
+          actualizarPuntos();
+        }, 3000);
+      }
+    });
+  });
+}
+
+// Call the createCups function to start the game
+createCups();
+
+
+function deleteCups() {
+  cupsContainer.innerHTML = '';
+}
+
+function playWinSound() {
+  const winSound = document.getElementById('winSound');
+  winSound.currentTime = 0; // Reinicia el audio a la posición inicial
+  winSound.play();
+}
+
+function playLoseSound() {
+  const loseSound = document.getElementById('loseSound');
+  loseSound.currentTime = 0;
+  loseSound.play();
+}
+
+function playCupSound() {
+  const cupSound = document.getElementById('cupSound');
+  cupSound.currentTime = 0;
+  cupSound.play();
+}
+
+function playCupMoveSound() {
+  const cupMoveSound = document.getElementById('cupMoveSound');
+  cupMoveSound.currentTime = 0;
+  cupMoveSound.play();
+}
+
+// Function to update user points using fetch
+async function updateUserPoints(pointsToAddOrSubtract) {
+  const apiUrl = "http://localhost/server/";
+  const requestOptions = {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      username: user.id,
+      points: pointsToAddOrSubtract,
+    }),
+  };
+
+  try {
+    response = await fetch(apiUrl, requestOptions)
+
+    if (response.ok) {
+      // Points updated successfully
+      console.log("Points updated successfully");
+      const id = user.id;
+      const points = await getUserPoints(id);
+      user = { id, points };
+      localStorage.setItem('user', JSON.stringify(user));
+    } else {
+      // Handle the error here
+      console.error("Error updating points");
+    }
+  }
+  catch (error) {
+    // Handle network or other errors
+    console.error("Error updating points:", error);
+  }
+}
+
+async function getUserPoints(username) {
+  const apiUrl = `http://localhost/server/?username=${username}`;
+
+  try {
+    const response = await fetch(apiUrl);
+    if (response.ok) {
+      const data = await response.json();
+      return data.points;
+    } else {
+      // Handle the error here
+      console.error("Error getting user points");
+      return null;
+    }
+  } catch (error) {
+    // Handle network or other errors
+    console.error("Error getting user points:", error);
+    throw error;
+  }
+}
+
+actualizarPuntos();
+async function actualizarPuntos() {
+  try {
+    const puntos = await getUserPoints(user.id);
+    document.getElementById("ptstotales__text").textContent = 'Puntos: ' + puntos;
+  } catch (error) {
+    console.error("Error al obtener los puntos:", error);
+  }
+}
